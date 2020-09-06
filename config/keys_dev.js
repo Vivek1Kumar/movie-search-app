ac9aa53d7e896782a8f0a5b443451593fa700154
System Administrator <vivekyadav264@gmail.com>
2020-09-06 17:35:50 +0530

    add config file

---
+++ config/keys_dev.js
@@ -0,0 +1,4 @@
+module.exports = {
+    api_endpoint: 'https://api.themoviedb.org',
+    api_key: 'e15259d0d33bb33c11a5a0f1a89a888a&language=en-US'
+  };
---
+++ config/keys_prod.js
@@ -0,0 +1,5 @@
+module.exports = {
+  api_endpoint: process.env.API_ENDPOINT,
+  api_key: process.env.API_KEY
+};
+
