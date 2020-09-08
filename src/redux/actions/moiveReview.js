ed434b231c82a7c272bf92d65502b7f450c83667
System Administrator <vivekyadav264@gmail.com>
2020-09-08 15:00:30 +0530

    remove comment code

--- src/redux/actions/moiveReview.js
+++ src/redux/actions/moiveReview.js
@@ -4,7 +4,7 @@ import config from '../../config/keys_dev';
 import {  GET_ERRORS, MOVIE_REVIEW, MOVIE_DETAILS, ADD_FAVOURITE, MOVIE_REVIEW_SEARCH } from './types';
 
 export const getMovieReviews = (pageNo) => dispatch => {
-	// axios.get(`https://api.themoviedb.org/3/movie/upcoming?api_key=e15259d0d33bb33c11a5a0f1a89a888a&language=en-US&page=${pageNo}`)
+
 	axios.get(`${config.api_endpoint}/3/movie/upcoming?api_key=${config.api_key}&language=en-US&page=${pageNo}`)
 		.then(res=> 
 			dispatch({
@@ -20,7 +20,7 @@ export const getMovieReviews = (pageNo) => dispatch => {
 }
 
 export const movieDetailsAction = (movie_id) => dispatch => {
-	// axios.get(`https://api.themoviedb.org/3/movie/${movie_id}?api_key=e15259d0d33bb33c11a5a0f1a89a888a&language=en-US`)
+
 	axios.get(`${config.api_endpoint}/3/movie/${movie_id}?api_key=${config.api_key}&language=en-US`)
 		.then(res=> 
 			dispatch({
@@ -35,7 +35,7 @@ export const movieDetailsAction = (movie_id) => dispatch => {
 		})
 }
 export const movieSearchAction = (movieName) => dispatch => {
-	// axios.get(`https://api.themoviedb.org/3/search/movie?api_key=e15259d0d33bb33c11a5a0f1a89a888a&language=en-US&page=1&include_adult=false&query=${movieName}`)
+
 	axios.get(`${config.api_endpoint}/3/search/movie?api_key=${config.api_key}&language=en-US&page=1&include_adult=false&query=${movieName}`)
 		.then(res=> 
 			dispatch({
